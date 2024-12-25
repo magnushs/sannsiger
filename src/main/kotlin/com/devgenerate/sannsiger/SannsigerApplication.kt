fcb75cf153c57d5381bf719474116a0370e72321
Magnus Skeide <magnus.hauge.skeide@entur.no>
2024-12-25 19:46:18 +0100

    Spring starter

---
+++ src/main/kotlin/com/devgenerate/sannsiger/SannsigerApplication.kt
@@ -0,0 +1,11 @@
+package com.devgenerate.sannsiger
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SannsigerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SannsigerApplication>(*args)
+}
---
+++ src/test/kotlin/com/devgenerate/sannsiger/SannsigerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devgenerate.sannsiger
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SannsigerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
